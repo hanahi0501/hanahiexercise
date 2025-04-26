72727c1ce055b962ed97ea8eb4fc9cd67382b1d9
hanahi0501 <huari0501@gmail.com>
2025-04-26 22:44:31 +0900

    ex8.1.py

---
+++ ex8.1.py
@@ -0,0 +1,13 @@
+formatter = "{} {} {} {} {} {} {} {} {}"
+
+print(formatter.format(
+"Let's talk about my fear.",
+"I am afraid of death.",
+"But when I think more,",
+"Losing everything doesn’t seem so scary.",
+"Maybe I don't fear death itself,",
+"But the pain before death.",
+"If I spend many years",
+"Being afraid of a little pain,",
+"It doesn’t make much sense."
+))
