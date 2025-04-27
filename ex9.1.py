35614c5781266b3d6faa162f9f0de0dbacf5f4d9
hanahi0501 <huari0501@gmail.com>
2025-04-27 15:16:02 +0900

    ex9.py

---
+++ ex9.1.py
@@ -0,0 +1,11 @@
+numbers = "1 2 3 4 5 6 7"
+fruits = "tomato\nbanana\napple\nmelon\nkiwi\npear"
+
+print("Here are the numbers: ",numbers)
+print("Here are the fruits: ",fruits)
+
+print("""
+Our sensei is a very good person.
+She is very gentle and funny.
+We looooooove her.
+""")
--- ex9.py
+++ ex9.py
@@ -1,7 +1,7 @@
 # Here's some new strange stuff, remember type it exactly.
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFed\nMar\nApr\nMay\nJun\nJul\nAug"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print("Here are the days: ", days)
 print("Here are the months: ", months)
@@ -11,4 +11,16 @@ There's something going on here.
 With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 Lines if we want, or 5, or 6.
+""")
+
+numbers = "1 2 3 4 5 6 7"
+fruits = "tomato\nbanana\napple\nmelon\nkiwi\npear"
+
+print("Here are the numbers: ",days)
+print("Here are the fruits: ",fruits)
+
+print("""
+Our sensei is a very good person.
+She is very gentle and funny.
+We looooooove her.
 """)
